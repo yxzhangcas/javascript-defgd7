138ce3ce75f0788eb4cb1515f1193c493d4e3e62
yxzhang <yxzhangcas@outlook.com>
2023-08-03 18:42:47 +0800

    Create 01-Console.js

---
+++ 01/01-Console.js
@@ -0,0 +1,19 @@
+// Web Browser -> F12 -> Console
+
+let primes = [2, 3, 5, 7];
+undefined
+primes[primes.length - 1];
+7
+primes[0] + primes[1];
+5
+function fact(x) {
+    if (x > 1) return x * fact(x - 1);
+    else return 1;
+}
+undefined
+fact(4);
+24
+fact(5);
+120
+fact(6);
+720
