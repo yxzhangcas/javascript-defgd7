70e1a8f3bb886802601087d934151d6221922087
yxzhang <yxzhangcas@outlook.com>
2023-08-03 18:44:50 +0800

    Create 02-Node.js

---
+++ 01/02-Node.js
@@ -0,0 +1,23 @@
+// PowerShell
+
+PS C:\Users\yxzha> node
+Welcome to Node.js v18.17.0.
+Type ".help" for more information.
+> .help
+.break    Sometimes you get stuck, this gets you out
+.clear    Alias for .break
+.editor   Enter editor mode
+.exit     Exit the REPL
+.help     Print this help message
+.load     Load JS from a file into the REPL session
+.save     Save all evaluated commands in this REPL session to a file
+
+Press Ctrl+C to abort current expression, Ctrl+D to exit the REPL
+> let x = 2, y = 3;
+undefined
+> x + y
+5
+> (x == 2) && (y == 3)
+true
+> (x > 2) || (y < 3)
+false
